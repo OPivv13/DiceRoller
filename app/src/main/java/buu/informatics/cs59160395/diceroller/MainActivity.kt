b371687b8ba9d54e2e2ae56521405c82ed2ba32c
OPivv13 <oranong32699@gmail.com>
2019-08-08 11:36:00 +0700

    Task: Change the text

--- app/src/main/java/buu/informatics/cs59160395/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/cs59160395/diceroller/MainActivity.kt
@@ -3,19 +3,29 @@ package buu.informatics.cs59160395.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import kotlin.contracts.contract
+
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollBottun: Button = findViewById(R.id.roll_button)
         rollBottun.setOnClickListener { rollDicr() }
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Roll"
     }
 
     private fun rollDicr(){
+        val randomInt = Random().nextInt(6) + 1
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
